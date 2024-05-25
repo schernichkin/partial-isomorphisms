526671617da538da92e5631ca86c898da948e6f4
Stanislav Chernichkin <schernichkin@gmail.com>
2024-05-25 15:48:43 +0300

    th-2.21

--- src/Control/Isomorphism/Partial/TH.hs
+++ src/Control/Isomorphism/Partial/TH.hs
@@ -21,37 +21,37 @@ conName (NormalC name _)   =   name
 conName (RecC name _)      =   name
 conName (InfixC _ name _)  =   name
 conName (ForallC _ _ con)  =   conName con
-conName (GadtC _ _ _)      =   gadtError
-conName (RecGadtC _ _ _)   =   gadtError
+conName (GadtC {})         =   gadtError
+conName (RecGadtC {})      =   gadtError
 
 -- | Extract the types of the constructor's fields.
 conFields :: Con -> [Type]
-conFields (NormalC _ fields)  =   map (\(_, t) -> t) fields
+conFields (NormalC _ fields)  =   map snd fields
 conFields (RecC _ fields)     =   map (\(_, _, t) -> t) fields
-conFields (InfixC lhs _ rhs)  =   map (\(_, t) -> t) [lhs, rhs]
+conFields (InfixC lhs _ rhs)  =   map snd [lhs, rhs]
 conFields (ForallC _ _ con)   =   conFields con
-conFields (GadtC _ _ _)       =   gadtError
-conFields (RecGadtC _ _ _)    =   gadtError
+conFields (GadtC {})          =   gadtError
+conFields (RecGadtC {})       =   gadtError
 
 -- Data dec information
 data DecInfo flag = DecInfo Type [TyVarBndr flag] [Con]
 
 -- | Extract data or newtype declaration information
-decInfo :: Dec -> Q (DecInfo ())
+decInfo :: Dec -> Q (DecInfo BndrVis)
 decInfo (DataD    _ name tyVars _ cs _) =  return $ DecInfo (ConT name) tyVars cs
 decInfo (NewtypeD _ name tyVars _ c _) =  return $ DecInfo (ConT name) tyVars [c]
 decInfo _ = fail "partial isomorphisms can only be derived for constructors of data type or newtype declarations."
 
 -- | Convert tyVarBndr to type
-tyVarBndrToType :: TyVarBndr () -> Type
+tyVarBndrToType :: TyVarBndr BndrVis -> Type
 tyVarBndrToType (PlainTV n _)   = VarT n
 tyVarBndrToType (KindedTV n _ k) = SigT (VarT n) k
 
 -- | Create Iso type for specified type and conctructor fields (Iso (a, b) (CustomType a b c))
-isoType :: Type -> [TyVarBndr ()] -> [Type] -> Q Type
+isoType :: Type -> [TyVarBndr BndrVis] -> [Type] -> Q Type
 isoType typ tyVarBndrs fields = do
     isoCon <- [t| Iso |]
-    return $ ForallT (map specified tyVarBndrs) [] $ isoCon `AppT` (isoArgs fields) `AppT` (applyAll typ $ map tyVarBndrToType tyVarBndrs)
+    return $ ForallT (map specified tyVarBndrs) [] $ isoCon `AppT` isoArgs fields `AppT` applyAll typ (map tyVarBndrToType tyVarBndrs)
     where
       specified (PlainTV name _) = PlainTV name SpecifiedSpec
       specified (KindedTV name _ kind) = KindedTV name SpecifiedSpec kind
@@ -76,30 +76,28 @@ constructorIso name = do
   isoFromCon (wildcard cs) con
 
 wildcard :: [Con] -> [MatchQ]
-wildcard cs
-  =  if length cs > 1
-     then  [match (wildP) (normalB [| Nothing |]) []]
-     else  []
+wildcard cs =  [match wildP (normalB [| Nothing |]) [] | length cs > 1]
 
 -- | Converts a constructor name (starting with an upper-case
 --   letter) into a function name (starting with a lower-case
 --   letter).
 rename :: Name -> Name
-rename n
-  = mkName (toLower c : cs) where c : cs = nameBase n
+rename n = mkName $ case nameBase n of 
+                      c:cs -> toLower c : cs
+                      a -> a
 
 defineIsomorphisms :: Name -> Q [Dec]
 defineIsomorphisms d = do
   TyConI dec  <-  reify d
   DecInfo typ tyVarBndrs cs          <-  decInfo dec
-  join `fmap` mapM (\a -> defFromCon (wildcard cs) typ tyVarBndrs a) cs
+  join `fmap` mapM (defFromCon (wildcard cs) typ tyVarBndrs) cs
 
 -- | Constructs a partial isomorphism definition for a
 --   constructor, given information about the constructor.
 --   The name of the partial isomorphisms is constructed by
 --   spelling the constructor name with an initial lower-case
 --   letter.
-defFromCon :: [MatchQ] -> Type -> [TyVarBndr ()] -> Con -> DecsQ
+defFromCon :: [MatchQ] -> Type -> [TyVarBndr BndrVis] -> Con -> DecsQ
 defFromCon matches t tyVarBndrs con = do
     let funName = rename $ conName con
     sig <- SigD funName `fmap` isoType t tyVarBndrs (conFields con)
@@ -119,9 +117,7 @@ isoFromCon matches con = do
                   [| Just $(foldl appE (conE c) vs) |]
   let g     =   lamE [varP v]
                   (caseE (varE v) $
-                    [ match (conP c ps)
-                        (normalB [| Just $(nested tupE vs) |]) []
-                    ] ++ matches)
+                    match (conP c ps) (normalB [| Just $(nested tupE vs) |]) [] : matches)
   [| Iso $f $g |]
 
 genPE :: Int -> Q ([PatQ], [ExpQ])
